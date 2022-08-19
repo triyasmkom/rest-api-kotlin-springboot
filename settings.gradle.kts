363438cf238650691b440a5a5db8db1045a559aa
triyasmkom <triyavian@gmail.com>
2022-08-19 10:58:17 +0700

    initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "resful-api-kotlin"
---
+++ src/main/kotlin/com/ths/resfulapikotlin/ResfulApiKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.ths.resfulapikotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResfulApiKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResfulApiKotlinApplication>(*args)
+}
